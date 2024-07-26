a998bc56e554a875478161609a0bb6ebb234d888
Ibraheem Hussain <ihussainprofessional@gmail.com>
2024-07-26 19:37:36 +0100

    Add files via upload
    Calculates the average and mode of a given list of numbers as well as sorting list. Mode calculator has is O(n) where n is the length of the list. Will also display multiple modes if there are more than one.

---
+++ MeanModeCalculator.py
@@ -0,0 +1,49 @@
+import random
+
+list = []
+
+for i in range (0, 1000):
+    x = random.randrange(1,1000)
+    list.append(x)
+
+blist = [1, 2, 2, 3, 3, 5, 5, 5]
+
+def average(list):
+    total = 0
+    listLen = len(list)
+    for i in range(0, listLen):
+        total += list[i]
+    return (total / listLen)
+
+def mode(list):
+    listLen = len(list)
+    list.sort()
+    modes = []
+    highest = 0
+    curr = 1
+    for i in range(0, listLen):
+        temp = list[i]
+        if i == (listLen - 1):
+            print("Reached End")
+        elif temp == list[i+1]:
+            curr += 1
+            if curr == highest:
+                modes.append(list[i])
+                highest = curr
+            if curr > highest:
+                modes = []
+                modes.append(list[i])
+                highest = curr
+        else:
+            curr = 1
+    return modes, highest
+
+avg = average(list)
+modeList, modeAmount = mode(list)
+
+print("The average was",avg)
+
+if len(modeList) > 1:
+    print("The mode was tied between:",modeList,"with each appearing",modeAmount,"times") 
+else:
+    print("The mode was",modeList[0],"which appeared",modeAmount,"times")
